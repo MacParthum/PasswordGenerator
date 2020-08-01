b3d14f6e521fef2a47448af2467787d8404c6088
Mac Parthum <macparthum@gmail.com>
2020-08-01 16:37:19 -0400

    added password length logic

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var generateBtn = document.querySelector("#generate");
 
-  function writePassword() {
-  var numberString = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() {
+    var numberString = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = numberString;
+    passwordText.value = numberString;
 
 }
 
@@ -16,45 +16,57 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 var symbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '[', ']', '{', '}'];
 
-var numberOfCharacters = parseInt(prompt('Password length?'))
-
-var pickedNumbersArr = []
-
-var numberString = ''
-
-
-
-if (confirm('Include lower case letters?')) {
-  pickedNumbersArr.push(lowerCase)
-}
-
-if (confirm('Include Upper case letters?')) {
-  pickedNumbersArr.push(upperCase)
-
-}
-
-if (confirm("Include numbers?")) {
-  pickedNumbersArr.push(numbers)
-
-}
-
-if (confirm("Include symbols?")) {
-  pickedNumbersArr.push(symbols)
-}
-
-console.log(numberOfCharacters, pickedNumbersArr)
-
-for (var i = 0; i < numberOfCharacters; i++) {
-  var randomNumber = Math.floor(Math.random() * pickedNumbersArr.length);
-  var randomXyzNumber = Math.floor(Math.random() * pickedNumbersArr[randomNumber].length);
-  var number = pickedNumbersArr[randomNumber][randomXyzNumber]
-  numberString += number
-  console.log(randomNumber, randomXyzNumber, number)
-}
-console.log(numberString)
-
 function generatePassword() {
-  return numberString
-}
 
+    var numberOfCharacters = parseInt(prompt('Password length?'))
+    console.log(numberOfCharacters)
+    if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+        alert ('Your password must be between 8 and 128 characters!')
+        return ''
+    }
+
+
+    var pickedNumbersArr = []
+
+    var numberString = ''
+
+
+
+    if (confirm('Include lower case letters?')) {
+        pickedNumbersArr.push(lowerCase)
+    }
+
+    if (confirm('Include Upper case letters?')) {
+        pickedNumbersArr.push(upperCase)
+
+    }
+
+    if (confirm("Include numbers?")) {
+        pickedNumbersArr.push(numbers)
+
+    }
+
+    if (confirm("Include symbols?")) {
+        pickedNumbersArr.push(symbols)
+    }
+
+    if (pickedNumbersArr.length === 0) {
+        alert('You must selet one character type')
+        return ''
+    }
+
+    console.log(numberOfCharacters, pickedNumbersArr)
+
+    for (var i = 0; i < numberOfCharacters; i++) {
+        var randomNumber = Math.floor(Math.random() * pickedNumbersArr.length);
+        var randomXyzNumber = Math.floor(Math.random() * pickedNumbersArr[randomNumber].length);
+        var number = pickedNumbersArr[randomNumber][randomXyzNumber]
+        numberString += number
+        console.log(randomNumber, randomXyzNumber, number)
+    }
+    console.log(numberString)
+
+    return numberString
+
+}
 generateBtn.addEventListener("click", writePassword);
